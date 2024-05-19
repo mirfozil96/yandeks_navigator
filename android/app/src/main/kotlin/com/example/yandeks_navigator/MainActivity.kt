06c9f3ef89d3fc97e5d3bffa2d7c7122cab9ccd9
mirfozil <99140314+mirfozil96@users.noreply.github.com>
2024-05-19 11:29:20 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/yandeks_navigator/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.yandeks_navigator
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("5d6635a4-578e-47dd-b911-7221fe7c81d2")
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
